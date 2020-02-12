e8329614f4a16468e3a95a67eb12d157d4a1faec
max6cn <max6cn@users.noreply.github.com>
2020-02-12 15:55:15 -0800

    Display book lists in gui text

--- week4/src/main/java/Api/BookApiImp.java
+++ week4/src/main/java/Api/BookApiImp.java
@@ -1,3 +1,5 @@
+package Api;
+
 import Repository.Author;
 import Repository.Book;
 import org.apache.http.HttpResponse;
@@ -11,77 +13,74 @@ import org.apache.logging.log4j.Logger;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
-public class BookApiImp implements BookApi{
+public class BookApiImp implements BookApi {
     public static final String BASE_URL = "https://www.gutenberg.org/";
     public static final String INDEX_ROOT = BASE_URL + "dirs/GUTINDEX.2020";
     private static final Logger logger = LogManager.getLogger(BookApi.class);
 
     HttpClient client = new DefaultHttpClient();
-    HttpGet request ;
-    HttpResponse response ;
+    HttpGet request;
+    HttpResponse response;
+
+    private static Book parseBook(String s) {
+        String[] parts = s.split(", by ");
+        String title = parts[0];
+        String[] nameparts = parts[1].split("[ \\t\\s]+");
+
+        StringBuilder authorBuilder = new StringBuilder();
+        Integer bookid = 9999;
+        for (int i = 0; i < nameparts.length - 1; i++) {
+            authorBuilder.append(" ").append(nameparts[i].trim());
+
+        }
+        try {
+            if (nameparts.length > 1) {
+                bookid = Integer.parseInt(nameparts[nameparts.length - 1]);
+
+            }
+        } catch (NumberFormatException e) {
+        }
+
+        Book b = new Book();
+        b.author = authorBuilder.toString().trim();
+        b.title = title;
+        b.Id = bookid;
+        return b;
+    }
 
     @Override
     public List<Book> getBooks() {
 
 //        String response =  getFileFromUrl(INDEX_ROOT);
-        String content = getFileFromFS("/Users/mark/IdeaProjects/MIS768/week4/GUTINDEX.ALL");
-        List<Book>  books = new ArrayList<>();
+        String content = getFileFromFS("/Users/mark/Projects/MIS768/week4/GUTINDEX.ALL");
+        List<Book> books = new ArrayList<>();
         String header = "TITLE and AUTHOR                                                     EBOOK NO.";
         int posStart = content.indexOf(header);
         content = content.substring(posStart + header.length());
         String[] lines = content.split("\\n");
 
-    books =
-        Stream.of(lines)
-            .filter(s -> s.contains(", by "))
-            .map(
-                s -> {
-                  String[] parts = s.split(", by ");
-                  String title = parts[0];
-                  String[] nameparts = parts[1].split("[ \\t\\s]+");
-
-                  StringBuilder authorBuilder = new StringBuilder();
-                    Integer bookid = 9999;
-                  for (int i = 0; i< nameparts.length -1 ; i++) {
-                      authorBuilder.append(" ").append(nameparts[i].trim());
-
-                  }
-                    try{
-                        if (nameparts.length > 1) {
-                            bookid = Integer.parseInt(nameparts[nameparts.length-1]);
-
-                        }
-
-                    } catch (NumberFormatException e) {
-//                        logger.error(e);
-                    }
-
-                  Book b = new Book();
-                  b.author = authorBuilder.toString().trim();
-                  b.title = title;
-                  b.Id = bookid;
-                  return b;
-                })
-            .collect(Collectors.toList());
+        books = Stream.of(lines)
+                .filter(s -> s.contains(", by "))
+                .map(
+                        BookApiImp::parseBook)
+                .collect(Collectors.toList());
         books.sort(Comparator.comparingInt(a -> a.Id));
-        logger.error(books);
         return books;
     }
 
     @Override
-    public  List<Author> getAuthors() {
-        String response =  getFileFromUrl(INDEX_ROOT);
-        List<Author>  authors = new ArrayList<>();
+    public List<Author> getAuthors() {
+        String response = getFileFromUrl(INDEX_ROOT);
+        List<Author> authors = new ArrayList<>();
         return authors;
 
     }
-    private String getFileFromFS(String path){
+
+    private String getFileFromFS(String path) {
         StringBuilder sb = new StringBuilder();
         try {
             FileInputStream inputStream = new FileInputStream(path);
@@ -89,10 +88,8 @@ public class BookApiImp implements BookApi{
                     (new InputStreamReader(inputStream));
             String line = "";
             while ((line = rd.readLine()) != null) {
-
-                sb.append(line + "\n");
+                sb.append(line).append("\n");
             }
-//            logger.trace(sb.toString());
         } catch (IOException e) {
             logger.error(e);
         }
@@ -121,8 +118,10 @@ public class BookApiImp implements BookApi{
         return sb.toString();
     }
 
-  public static void main(String[] args) {
-      BookApi api = new BookApiImp();
-      api.getBooks();
-  }
+    public static void main(String[] args) {
+        BookApi api = new BookApiImp();
+        List<Book> books = api.getBooks();
+        logger.error(books);
+
+    }
 }
